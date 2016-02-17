85a44de3263dd68b8f598893692052a26120dd0f
Kyle Lin <kylelin47@gmail.com>
2016-02-17 15:53:59 -0500

    Some basic gravity and drag stuff

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public bool GetInteractionKey() {
+		return (Input.GetKeyDown (KeyCode.Space) || Input.GetButtonDown("Fire1"));
+	}
+}
---
+++ Assets/Scripts/GravityController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravityController : MonoBehaviour {
+	private Rigidbody rbody;
+	private GameController gc;
+	// Use this for initialization
+	void Start () {
+		rbody = GetComponent<Rigidbody> ();
+		gc = (GameController) FindObjectOfType(typeof(GameController));
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// at the right height, hit the key
+		if (gc.GetInteractionKey () && transform.position.y < 10000) {
+			DeployParachute ();
+		}
+		// hit the water
+		if (transform.position.y < 5) {
+			rbody.useGravity = false;
+		}
+	}
+
+	void DeployParachute() {
+		rbody.drag = 20;
+	}
+}
