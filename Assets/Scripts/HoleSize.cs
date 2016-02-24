b3752cf90c1f4f67939c001d3628c73cdd564598
Virada Long <viradalong16@ufl.edu>
2016-02-24 09:20:49 -0500

    terrain

---
+++ Assets/Scripts/HoleSize.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HoleSize : MonoBehaviour {
+    public Renderer rend;
+    // Use this for initialization
+    void Start () {
+        rend = GetComponent<Renderer>();
+        float radius = rend.bounds.extents.x;
+        print(radius);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
