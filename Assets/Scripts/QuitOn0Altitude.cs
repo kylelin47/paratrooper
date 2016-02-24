3492f9349a805457287e21d7a6c98d54a362daff
Virada Long <viradalong16@ufl.edu>
2016-02-24 15:32:35 -0500

    added quit on 0 altitude

---
+++ Assets/Scripts/QuitOn0Altitude.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuitOn0Altitude : MonoBehaviour {
+    private GameController gc;
+    // Use this for initialization
+    void Start () {
+        gc = (GameController)FindObjectOfType(typeof(GameController));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if((gc.playerAltitude * 3.28) < 0  )
+        {
+            print("Game has quit technically");
+            Application.Quit(); //quit game when altitude is 0
+        }
+	}
+}
